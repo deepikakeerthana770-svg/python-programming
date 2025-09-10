2ab178cb8ca86e9ef0bd1aa45a4cbe7b8fd5eb19
Deepika M <deepikakeerthana770@gmail.com>
2025-09-10 17:35:20 +0530

    Add files via upload

---
+++ empty list.py
@@ -0,0 +1,22 @@
+a=[]
+n=int(input("enter a number of elements"))
+for i in range(0,n):
+    m=int(input("enter number"))
+    a.append(m)
+print(a)
+print(a[0])
+print(a[-1])
+print(a[0:3])
+print(a[-3:])
+print(a[1:3])
+a.extend([6,7,8])
+print(a)
+a.insert(0,3)
+print(a)
+
+    
+    
+       
+    
+
+ 
